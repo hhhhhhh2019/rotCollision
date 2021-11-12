b03732e0f4a998e668b90b41894560983b446153
hhhhhhh2019 <47990344+hhhhhhh2019@users.noreply.github.com>
2021-11-12 17:01:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const width = 360;
-const height = 360;
+const width = 480;
+const height = 480;
 
 const cnv = document.getElementById("cnv");
 cnv.width = width;
@@ -8,7 +8,7 @@ cnv.height = height;
 const ctx = cnv.getContext("2d");
 
 
-const vec = function(x, y) {
+const vec = function(x, y=x) {
 	return {x, y};
 }
 
@@ -31,11 +31,34 @@ const rotate = function(v, a) {
 
 const swap = function(a,b) {let tmp=a; a=b; b=a;};
 
+const sum = function(a, b) {return vec(a.x + b.x, a.y + b.y)};
+const sub = function(a, b) {return vec(a.x - b.x, a.y - b.y)};
+const mul = function(a, b) {return vec(a.x * b.x, a.y * b.y)};
+const div = function(a, b) {return vec(a.x / b.x, a.y / b.y)};
+
+const dot = function(a, b) {
+	return a.x * b.x + a.y * b.y;
+}
+
+const proj = function(p, d) {
+	//D = A + AB * Dot(AC, AB) / Dot(AB, AB)
+	//D = d * dot(p, d) / dot(d, d);
+
+	return mul(div(
+		vec(dot(p, d)),
+		vec(dot(d, d))
+	), d);
+
+	//return sum(vec(0,0), div(mul(d, vec(dot(p, d))), vec(dot(d, d))));
+}
+
 const proj4p2x = function(a,b,c,d, angle) {
-	let ap = rotate(a, -angle).x;
-	let bp = rotate(b, -angle).x;
-	let cp = rotate(c, -angle).x;
-	let dp = rotate(d, -angle).x;
+	let axis = rotate(vec(1,0), angle);
+
+	let ap = rotate(proj(a, axis), -angle).x;
+	let bp = rotate(proj(b, axis), -angle).x;
+	let cp = rotate(proj(c, axis), -angle).x;
+	let dp = rotate(proj(d, axis), -angle).x;
 	
 	return [
 		Math.min(Math.min(bp, Math.min(cp, dp)), ap),
@@ -44,10 +67,12 @@ const proj4p2x = function(a,b,c,d, angle) {
 }
 
 const proj4p2y = function(a,b,c,d, angle) {
-	let ap = rotate(a, -angle).y;
-	let bp = rotate(b, -angle).y;
-	let cp = rotate(c, -angle).y;
-	let dp = rotate(d, -angle).y;
+	let axis = rotate(vec(0,1), angle);
+
+	let ap = rotate(proj(a, axis), -angle).y;
+	let bp = rotate(proj(b, axis), -angle).y;
+	let cp = rotate(proj(c, axis), -angle).y;
+	let dp = rotate(proj(d, axis), -angle).y;
 	
 	return [
 		Math.min(Math.min(bp, Math.min(cp, dp)), ap),
@@ -72,7 +97,7 @@ class Rect {
 		ctx.rotate(this.angle);
 		
 		ctx.strokeStyle = this.color;
-		ctx.lineWidth = 1;
+		ctx.lineWidth = 2;
 		ctx.strokeRect(-this.size.x / 2, -this.size.y / 2,
 					 this.size.x, this.size.y);
 		
@@ -83,36 +108,30 @@ class Rect {
 		for (let i of objs) {
 			if (i == this) continue;
 			
-			let ap1 = vec(-this.size.x / 2 + this.pos.x - i.pos.x,
-						  -this.size.y / 2 + this.pos.y - i.pos.y);
-			let ap2 = vec(this.size.x / 2 + this.pos.x - i.pos.x,
-						  -this.size.y / 2 + this.pos.y - i.pos.y);
-			let ap3 = vec(this.size.x / 2 + this.pos.x - i.pos.x,
-						  this.size.y / 2 + this.pos.y - i.pos.y);
-			let ap4 = vec(-this.size.x / 2 + this.pos.x - i.pos.x,
-						  this.size.y / 2 + this.pos.y - i.pos.y);
+			let ap1 = sub(sum(rotate(vec(-this.size.x / 2,
+										 -this.size.y / 2), this.angle), this.pos), i.pos);
+			let ap2 = sub(sum(rotate(vec(this.size.x / 2,
+										 -this.size.y / 2), this.angle), this.pos), i.pos);
+			let ap3 = sub(sum(rotate(vec(this.size.x / 2,
+										 this.size.y / 2), this.angle), this.pos), i.pos);
+			let ap4 = sub(sum(rotate(vec(-this.size.x / 2,
+										 this.size.y / 2), this.angle), this.pos), i.pos);
 			
-			let bp1 = vec(-i.size.x / 2 + i.pos.x - this.pos.x,
-						  -i.size.y / 2 + i.pos.y - this.pos.y);
-			let bp2 = vec(i.size.x / 2 + i.pos.x - this.pos.x,
-						  -i.size.y / 2 + i.pos.y - this.pos.y);
-			let bp3 = vec(i.size.x / 2 + i.pos.x - this.pos.x,
-						  i.size.y / 2 + i.pos.y - this.pos.y);
-			let bp4 = vec(-i.size.x / 2 + i.pos.x - this.pos.x,
-						  i.size.y / 2 + i.pos.y - this.pos.y);
+			let bp1 = sub(sum(rotate(vec(-i.size.x / 2,
+								 -i.size.y / 2), i.angle), i.pos), this.pos);
+			let bp2 = sub(sum(rotate(vec(i.size.x / 2,
+								 -i.size.y / 2), i.angle), i.pos), this.pos);
+			let bp3 = sub(sum(rotate(vec(i.size.x / 2,
+								 i.size.y / 2), i.angle), i.pos), this.pos);
+			let bp4 = sub(sum(rotate(vec(-i.size.x / 2,
+								 i.size.y / 2), i.angle), i.pos), this.pos);
 			
 			
 			let abx = proj4p2x(ap1, ap2, ap3, ap4, i.angle);
 			let aby = proj4p2y(ap1, ap2, ap3, ap4, i.angle);
 			
-			//abx[0] += this.pos.x - i.pos.x; abx[1] += this.pos.x - i.pos.x;
-			//aby[0] += this.pos.y - i.pos.y; aby[1] += this.pos.y - i.pos.y;
-			
 			let bax = proj4p2x(bp1, bp2, bp3, bp4, this.angle);
 			let bay = proj4p2y(bp1, bp2, bp3, bp4, this.angle);
-			
-			//bax[0] += i.pos.x - this.pos.x; bax[1] += i.pos.x - this.pos.x;
-			//bay[0] += i.pos.y - this.pos.y; bay[1] += i.pos.y - this.pos.y;
 			
 			
 			ctx.save();
@@ -121,7 +140,7 @@ class Rect {
 			ctx.rotate(this.angle);
 			
 			ctx.strokeStyle = "green";
-			ctx.lineWidth = 1;
+			ctx.lineWidth = 2;
 			
 			ctx.beginPath();
 			ctx.moveTo(-50, 0);
@@ -129,7 +148,7 @@ class Rect {
 			ctx.stroke();
 			
 			ctx.strokeStyle = this.color;
-			ctx.lineWidth = 2;
+			ctx.lineWidth = 3;
 			
 			ctx.beginPath();
 			ctx.moveTo(bax[0], 0);
@@ -143,7 +162,7 @@ class Rect {
 			ctx.rotate(this.angle);
 			
 			ctx.strokeStyle = "yellow";
-			ctx.lineWidth = 0.5;
+			ctx.lineWidth = 2;
 			
 			ctx.beginPath();
 			ctx.moveTo(0, -50);
@@ -161,15 +180,15 @@ class Rect {
 			
 			
 			
-			/*if (abx[0] > i.size.x / 2 + i.pos.x) continue;
-			if (abx[1] < -i.size.x / 2 + i.pos.x) continue;
-			if (aby[0] > i.size.y / 2 + i.pos.y) continue;
-			if (aby[1] < -i.size.y / 2 + i.pos.y) continue;*/
+			if (abx[0] > i.size.x / 2) continue;
+			if (abx[1] < -i.size.x / 2) continue;
+			if (aby[0] > i.size.y / 2) continue;
+			if (aby[1] < -i.size.y / 2) continue;
 			
-			/*if (bax[0] < -this.size.x / 2 + this.pos.x) continue;
-			if (bax[1] > this.size.x / 2 + this.pos.x) continue;*/
-			if (bay[0] > this.size.y / 2 + this.pos.y) continue;
-			if (bay[1] < -this.size.y / 2 + this.pos.y) continue;
+			if (bax[0] > this.size.x / 2) continue;
+			if (bax[1] < -this.size.x / 2) continue;
+			if (bay[0] > this.size.y / 2) continue;
+			if (bay[1] < -this.size.y / 2) continue;
 			
 			return true;
 		}
@@ -191,8 +210,8 @@ class Rect {
 
 
 const objs = [
-	new Rect(vec(170,120), vec(10,10), 0, vec(0,0), "red"),
-	new Rect(vec(170,180), vec(50,10), 0, vec(0,0), "blue")
+	new Rect(vec(170,120), vec(20,20), 0, vec(0,1), "red"),
+	new Rect(vec(170,180), vec(90,20), 0, vec(0,0), "blue")
 ];
 
 const update = function() {
@@ -201,9 +220,8 @@ const update = function() {
 	for (let i of objs) {
 		i.move(objs);
 		i.draw();
+		i.angle += Math.PI * 0.001;
 	}
-	
-	objs[0].angle += 0.001;
 	
 	requestAnimationFrame(update);
 }
